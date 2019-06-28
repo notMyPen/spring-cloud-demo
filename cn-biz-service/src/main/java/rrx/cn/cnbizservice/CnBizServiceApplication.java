29eaa1d698aeb7eb76c4e5d1c2eda4a193201921
xuhongyu <xuhongyu@jinjiedao.com>
2019-06-28 09:56:31 +0800

    添加biz

---
+++ cn-biz-service/src/main/java/rrx/cn/cnbizservice/CnBizServiceApplication.java
@@ -0,0 +1,22 @@
+package rrx.cn.cnbizservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
+//@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class CnBizServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnBizServiceApplication.class, args);
+	}
+
+}
